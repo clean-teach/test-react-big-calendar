a1f5daf19e7c263526f2ead1aba2369bd08c1aa5
ch <82921898+clean-teach@users.noreply.github.com>
2024-11-20 15:46:37 +0900

    Test : react-big-calendar 자체 문제인지 단위 테스트
    - 아무런 커스터마이징 없이, 최초 설치만 한 상태에서
    - 목업 데이터만을 입력하여 테스트 하여 본다.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 function App() {
+  const localizer = momentLocalizer(moment);
+  const myEventsList = [
+    {
+      classSeq: 1,
+      title: "RW2-MA2(Adaptive)",
+      start: "2024-09-25",
+      end: "2024-10-02",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "RW4-MA4(Adaptive)",
+      start: "2024-09-25",
+      end: "2024-10-02",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "RW5-MA5(Adaptive)",
+      start: "2024-10-01",
+      end: "2024-10-08",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "RW7-MA7(Adaptive)",
+      start: "2024-10-01",
+      end: "2024-10-08",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "RW45-MA45(Adaptive)",
+      start: "2024-11-01",
+      end: "2024-11-08",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "Level Test",
+      start: "2024-10-30",
+      end: "2024-11-06",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 1,
+      title: "RW50-MA50(Adaptive)",
+      start: "2024-11-02",
+      end: "2024-11-10",
+      classColor: "green",
+      allDay: true
+    },
+    {
+      classSeq: 63,
+      title: "MA55(Hard)",
+      start: "2024-10-31",
+      end: "2024-11-03",
+      classColor: "purple",
+      allDay: true
+    }
+  ];
+
+  console.log(`🚨 myEventsList :`, myEventsList, `🚨`);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 }
